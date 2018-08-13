d62b6dcf02fd561a63ed6f15ee57642d423904f5
David Levy <david.g.levy@gmail.com>
2018-08-13 12:45:32 +1000

    Added missing system exit statements

--- src/main/java/com/cloudera/hdfsutil/HdfsTar.java
+++ src/main/java/com/cloudera/hdfsutil/HdfsTar.java
@@ -24,6 +24,8 @@ public class HdfsTar {
 	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("hdfs-tar [source-tar-gz] [dest-folder]");
+        	System.exit(1);
+
 		} else {
 			System.out.println("Extracting [" + args[0] + "] to [" + args[1] + "]");
 		}
