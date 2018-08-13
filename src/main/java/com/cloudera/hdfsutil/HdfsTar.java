3ddea7843f0c6368387826b8064a73d3e0c42f79
David Levy <david.g.levy@gmail.com>
2018-08-13 12:36:15 +1000

    Added debug logging

--- src/main/java/com/cloudera/hdfsutil/HdfsTar.java
+++ src/main/java/com/cloudera/hdfsutil/HdfsTar.java
@@ -61,9 +61,13 @@ public class HdfsTar {
 	        Path base = new Path(destination);
 	        if (!fs.exists(base)) {
 	        	throw new IllegalArgumentException("Path doesn't exist [" + destination + "]");
+	        } else {
+	        	System.out.println("Destination exists");
 	        }
 	        
 	        if (fs.isDirectory(base)) {
+	        	System.out.println("Destination is a directory");
+	        	
 	        	int count = 0;
 	        	RemoteIterator<LocatedFileStatus> files = fs.listFiles(base, false);
 	        	while (files.hasNext()) {
@@ -74,6 +78,8 @@ public class HdfsTar {
 	        	
 	        	if (count > 0) {
 	        		throw new IllegalArgumentException("Destination [" + destination + "] is not empty");
+	        	} else {
+	        		System.out.println("Destination is empty");
 	        	}
 	        	
 	        } else {
@@ -82,10 +88,13 @@ public class HdfsTar {
 	        
 	        while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
 	            /** If the entry is a directory, create the directory. **/
-	            if (entry.isDirectory()) {
-	                
-	            	Path src = new Path(destination + "/" + entry.getName());
+            	String pathText = destination + "/" + entry.getName();
+
+            	
+	        	if (entry.isDirectory()) {
+	        		System.out.println("Processing path [" + pathText + "] as directory");
 	            	
+	            	Path src = new Path(pathText);
 	            	
 	            	boolean created = fs.mkdirs(src); 
 	            	
@@ -94,7 +103,9 @@ public class HdfsTar {
 	                            src.toString());
 	                }
 	            } else {
-	                Path newFile = new Path(destination + "/" + entry.getName());
+	        		System.out.println("Processing path [" + pathText + "] as file");
+	            	
+	                Path newFile = new Path(pathText);
 	                
 	                try (FSDataOutputStream output = fs.create(newFile)) {
 	                	IOUtils.copy(tarIn, output);
